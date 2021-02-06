6eb23d63906da1661f1c265a336ab5a23c867c43
Nino <n.popovic@live.com>
2021-02-06 20:14:47 +0100

    add quantity button, fix checkout product card styling

--- src/components/checkout/CheckoutProductCard.js
+++ src/components/checkout/CheckoutProductCard.js
@@ -3,6 +3,10 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+// materialUI icons imports
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 
 import picture2 from "../../AV6243-400_350_350px.webp";
 
@@ -14,12 +18,11 @@ const useStyles = makeStyles((theme) => ({
   containerStyle: {
     display: "flex",
     flexDirection: "column",
-    borderTop: "1px solid black",
-    borderBottom: "1px solid black",
     minHeight: "200px",
     marginTop: "20px",
+    borderBottom: "1px solid black",
     justifyContent: "space-between",
-    [theme.breakpoints.up("sm")]: {
+    [theme.breakpoints.up("md")]: {
       flexDirection: "row",
     },
   },
@@ -27,12 +30,33 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    textAlign: "center",
+    [theme.breakpoints.up("lg")]: {
+      textAlign: "start",
+    },
+  },
+  quantityTypographyStyle: {
+    display: "flex",
+    alignItems: "center",
+  },
+  quantityDivStyle: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  },
+  iconButtonStyle: {
+    "&:hover": {
+      backgroundColor: "#fff",
+    },
   },
   imageDivStyle: {
-    minWidth: "270px",
+    minWidth: "230px",
     minHeight: "200px",
     backgroundSize: "270px 200px",
-    background: ` url(${picture2}) no-repeat`,
+    background: ` url(${picture2}) no-repeat center`,
   },
 }));
 
@@ -48,7 +72,7 @@ export default function CheckoutProductCard(props) {
   return (
     <Grid className={`w-100 ${classes.containerStyle}`}>
       <Grid item className={classes.imageDivStyle} xl={5} sm={12}></Grid>
-      <Grid item className={classes.priceDivStyle} xl={5} sm={12}>
+      <Grid item className={classes.priceDivStyle} xl={4} sm={12}>
         <Typography variant="body2" color="textSecondary" component="p">
           Brand : {productBrand}
         </Typography>
@@ -59,10 +83,31 @@ export default function CheckoutProductCard(props) {
           Price : {productPrice} $
         </Typography>
       </Grid>
-      <Grid item className={classes.priceDivStyle} xl={2} sm={12}>
-        <Typography variant="h6" color="textSecondary" component="h6">
+      <Grid item className={classes.quantityDivStyle} xl={3} sm={12}>
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          component="h6"
+          className={classes.quantityTypographyStyle}
+        >
           Quantity : 1
         </Typography>
+        <div className={classes.quantityTypographyStyle}>
+          <IconButton
+            aria-label="increase-quantity"
+            size="medium"
+            className={classes.iconButtonStyle}
+          >
+            <ArrowUpwardIcon fontSize="inherit" />
+          </IconButton>
+          <IconButton
+            aria-label="decrease-quantity"
+            size="medium"
+            className={classes.iconButtonStyle}
+          >
+            <ArrowDownwardIcon fontSize="inherit" />
+          </IconButton>
+        </div>
       </Grid>
     </Grid>
   );
